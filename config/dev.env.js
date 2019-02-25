7ff7ea2ada03c0570fd97c2f62bcae516c3fac2c
Wendy Lee <949970927@qq.com>
2019-02-25 15:22:57 +0800

    项目搭建

---
+++ config/dev.env.js
@@ -0,0 +1,8 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  url: '"http://10.45.144.160:8091"'
+})
